532d203ca99e423254525731ff1095e11441ee64
azmalent <dmitrysjestov@niuitmo.ru>
2021-02-04 00:24:11 +0300

    Added Chinese localization (unluckylin)

--- src/main/java/azmalent/potiondescriptions/PotionDescriptions.java
+++ src/main/java/azmalent/potiondescriptions/PotionDescriptions.java
@@ -28,7 +28,7 @@ public class PotionDescriptions
 {
     public static final String MODID = "potiondescriptions";
     public static final String NAME = "Potion Descriptions";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
 
     public static Logger logger;
 
